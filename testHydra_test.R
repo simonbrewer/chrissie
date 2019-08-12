23cca8330b28064cca7820254b51e22cc05ed616
Simon Brewer <brewersi@gmail.com>
2019-08-12 15:24:12 -0600

    Updated test case to use more sensible values

--- testHydra_test.R
+++ testHydra_test.R
@@ -18,7 +18,7 @@ source("helpers.R")
 
 ###############################################################################
 ## Lake border
-cllake = readOGR("./chrissie_lake/chrissie_lake.shp")
+# cllake = readOGR("./chrissie_lake/chrissie_lake.shp")
 
 ###############################################################################
 ## MODEL SETUP
@@ -29,23 +29,26 @@ bpf = 0 ## Proportion of runoff to put in baseflow [0-1]
 effvol = 0.3 
 
 ## Base files
-# myext = extent(c(30,31,-27,-26))
-# dem.r = crop(raster("./dem/chrissie_dem.nc"), myext)
-# bas.r = crop(raster("./dem/chrissie_bas.nc"), myext)
+oldw <- getOption("warn")
+options(warn = -1)
+
+dem.r = raster("./dem/test_dem.nc")
+myext = extent(dem.r)
+bas.r = raster("./dem/test_bas.nc")
+ldd.r = raster("./dem/test_ldd.nc")
+mflac.r = raster("./dem/test_mflac.nc")
+iout.r = raster("./dem/test_iout.nc")
+jout.r = raster("./dem/test_jout.nc")
+
+options(warn = oldw)
+
+# dem.r = raster("./dem/chrissie_dem.nc")
+# bas.r = raster("./dem/chrissie_bas.nc")
 # ldd.r = raster("~/Documents/grassdata/hydrosheds/bclake/af_ldd_30g.nc")
 # ldd.r = crop(ldd.r, extent(dem.r))
-# mflac.r = crop(raster("./dem/cllake_mflac.nc"), myext)
-# iout.r = crop(raster("./dem/cllake_iout.nc"), myext)
-# jout.r = crop(raster("./dem/cllake_jout.nc"), myext)
-
-dem.r = raster("./dem/chrissie_dem.nc")
-bas.r = raster("./dem/chrissie_bas.nc")
-ldd.r = raster("~/Documents/grassdata/hydrosheds/bclake/af_ldd_30g.nc")
-ldd.r = crop(ldd.r, extent(dem.r))
-mflac.r = raster("./dem/cllake_mflac.nc")
-iout.r = raster("./dem/cllake_iout.nc")
-jout.r = raster("./dem/cllake_jout.nc")
-myext = extent(dem.r)
+# mflac.r = raster("./dem/cllake_mflac.nc")
+# iout.r = raster("./dem/cllake_iout.nc")
+# jout.r = raster("./dem/cllake_jout.nc")
 
 ## Replace nulls
 dem.r[is.na(dem.r)] <- 0
@@ -56,7 +59,8 @@ iout.r[is.na(iout.r)] <- 0
 jout.r[is.na(jout.r)] <- 0
 
 ## Clip lake basin
-mask.r = bas.r == 18872 ## 18872 is the basin corresponding to Chrissie lake
+# mask.r = bas.r == 18872 ## 18872 is the basin corresponding to Chrissie lake
+mask.r = dem.r > 0
 
 ###############################################################################
 ## Estimate cell areas
@@ -68,29 +72,36 @@ gridx = dim(dem.r)[1]
 gridy = dim(dem.r)[2]
 
 ###############################################################################
-## Assign outlet cell
-out.x = 20.364
-out.y = -30.88
-out.sp = SpatialPoints(cbind(out.x,out.y))
-out.cell <- cellFromXY(dem.r, out.sp)
-outlet.r = setValues(dem.r, 0)
-outlet.r[out.cell] <- 1
-
-###############################################################################
 ## Forcing data
-dpre.stk = crop(brick("./inputs/dpre_cl.nc"), myext)
-# dpre.stk[is.na(dpre.stk[])] <- 0
+dpre.stk = crop(brick("./inputs/dpre_cl.nc"), myext) * 2
+dpre.stk[is.na(dpre.stk)] <- 0
 dpet.stk = crop(brick("./inputs/dpet_cl.nc"), myext)
-# dpet.stk[is.na(dpet.stk[])] <- 0
+dpet.stk[is.na(dpet.stk)] <- 0
 devp.stk = crop(brick("./inputs/devp_cl.nc"), myext)
-# devp.stk[is.na(devp.stk[])] <- 0
+devp.stk[is.na(devp.stk)] <- 0
 dcn.stk = crop(brick("./inputs/dcn_cl.nc"), myext)
-# dcn.stk[is.na(dcn.stk[])] <- 0
+dcn.stk[is.na(dcn.stk)] <- 0
 
 ###############################################################################
 ## Calculate runoff
 dro.stk = (dpre.stk + dcn.stk) - dpet.stk
 
+## Set constant values
+dro.stk[] <- 1
+dpre.stk[] <- 1
+devp.stk[] <- 0
+area.r [] <- 1000
+
+## Plot 'em
+dpre.reg = cellStats(dpre.stk, mean)
+dpet.reg = cellStats(dpet.stk, mean)
+devp.reg = cellStats(devp.stk, mean)
+dro.reg = cellStats(dro.stk, mean)
+
+plot(dpre.reg, type='l', ylim=c(0,7))
+lines(dpet.reg, col=2)
+lines(devp.reg, col=3)
+lines(dro.reg, col=4)
 ###############################################################################
 ## Water storage rasters
 wse.r = wvl.r = war.r = setValues(dem.r, 0)
@@ -112,11 +123,8 @@ wse = as.matrix(wse.r)
 war = as.matrix(war.r)
 
 pre = as.array(dpre.stk)
-pre[which(is.na(pre), arr.ind = TRUE)] <- 0
 evp = as.array(devp.stk)
-evp[which(is.na(evp), arr.ind = TRUE)] <- 0
 ro = as.array(clamp(dro.stk, lower=0, useValues=TRUE))
-ro[which(is.na(ro), arr.ind = TRUE)] <- 0
 sro = ro * (1-bpf)
 bro = ro * bpf
 
@@ -127,10 +135,11 @@ bro = ro * bpf
 cols <- colorRampPalette(brewer.pal(9,"Blues"))(100)
 
 nyrs = 1
+nspin = 1
 ndays = 365
 
 sim.out = rhydra(gridx, gridy, nyrs, ndays, startyear=1, 
-                 res=30, converg=0, laket=0, spin=50,
+                 res=30, converg=1, laket=0, spin=nspin,
                  dem=dem, mask=mask, area=cella, rivdir=ldd, mflac=mflac,
                  outnewi=iout, outnewj=jout, basin=bas, 
                  prcpi=pre, evapi=evp, runin=sro, drainin=bro) 
@@ -138,16 +147,14 @@ sim.out = rhydra(gridx, gridy, nyrs, ndays, startyear=1,
 tmp.r = setValues(dem.r, matrix(sim.out$outelv, 
                                 nrow=dim(dem.r)[1], ncol=dim(dem.r)[2]))
 plot(tmp.r - dem.r)
-plot(cllake, add=TRUE)
 
 tmp.r = setValues(dem.r, matrix(sim.out$larea, 
                                 nrow=dim(dem.r)[1], ncol=dim(dem.r)[2]))
 plot(tmp.r)
-plot(cllake, add=TRUE)
-
-tmp.r = setValues(dem.r, matrix(sim.out$voll, 
+stop()
+tmp.r = setValues(dem.r, matrix(sim.out$volr, 
                                 nrow=dim(dem.r)[1], ncol=dim(dem.r)[2]))
-plot(tmp.r, col=RColorBrewer::brewer.pal(7, "Blues"))
+plot(tmp.r)
 plot(cllake, add=TRUE)
 
 
